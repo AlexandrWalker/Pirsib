13984adb67293c8f1730df4661e9140efb4c48f0
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-10-21 21:32:12 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
       offset: -60,
     },
   });
-
   gsap.ticker.add((time) => {
     lenis.raf(time * 1000);
   });
@@ -76,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function headerFunc() {
     const header = document.getElementById('header');
     const firstSection = document.querySelector('section');
-    const marker = 150;
+    const marker = 10;
     let lastScrollTop = 1;
     const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
 
@@ -103,15 +102,21 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Градиент текста по курсору
    */
-  const gradientMoves = document.querySelectorAll('[data-gradient="gradientMove"]');
-  gradientMoves.forEach(gradientMove => {
-    window.addEventListener('mousemove', e => {
-      w = window.innerWidth;
-      h = window.innerHeight;
-      x = Math.round(e.pageX / w * 100);
-      y = Math.round(e.pageY / h * 100);
-      gradientMove.style.backgroundImage = `radial-gradient(42.06% 481.38% at ${x}% ${y}%, #AD32AE 0%, #3C49C2 100%)`;
-    });
+  $(window).on('resize load', function () {
+    if (window.innerWidth > 834) {
+
+      const gradientMoves = document.querySelectorAll('[data-gradient="gradientMove"]');
+      gradientMoves.forEach(gradientMove => {
+        window.addEventListener('mousemove', e => {
+          w = window.innerWidth;
+          h = window.innerHeight;
+          x = Math.round(e.pageX / w * 100);
+          y = Math.round(e.pageY / h * 100);
+          gradientMove.style.backgroundImage = `radial-gradient(42.06% 481.38% at ${x}% ${y}%, #AD32AE 0%, #3C49C2 100%)`;
+        });
+      });
+
+    }
   });
 
   /**
@@ -157,108 +162,245 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Инициализация слайдера
    */
-  const opinionSlider = new Swiper('.opinion--slider', {
-    slidesPerGroup: 1,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    grabCursor: true,
-    simulateTouch: true,
-    watchOverflow: true,
-    speed: 600,
-    mousewheel: {
-      forceToAxis: true,
-      sensitivity: 1,
-      releaseOnEdges: true,
-    },
-    touchEvents: {
-      prevent: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: false,
-    breakpoints: {
-      835: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        pagination: false,
-        navigation: {
-          prevEl: ".opinion-button-prev",
-          nextEl: ".opinion-button-next",
+  const swiper = document.querySelector('.swiper');
+  if (swiper) {
+    const opinionSlider = new Swiper('.opinion__slider', {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      simulateTouch: true,
+      watchOverflow: true,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+        sensitivity: 1,
+        releaseOnEdges: true,
+      },
+      touchEvents: {
+        prevent: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        prevEl: ".opinion-button-prev",
+        nextEl: ".opinion-button-next",
+      },
+      breakpoints: {
+        835: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+          pagination: false,
         },
       },
-    },
-  });
+    });
 
-  const personalSlider = new Swiper('.personal__slider', {
-    slidesPerGroup: 1,
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    grabCursor: true,
-    simulateTouch: true,
-    watchOverflow: true,
-    speed: 1000,
-    // effect: "fade",
-    // effect: "flip",
-    effect: "creative",
-    creativeEffect: {
-      prev: {
-        translate: ["-20%", 0, -1],
-      },
-      next: {
-        translate: ["20%", 0, 0],
-      },
-    },
-    mousewheel: {
-      forceToAxis: true,
-      sensitivity: 1,
-      releaseOnEdges: true
-    },
-    touchEvents: {
-      prevent: true
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-  });
-
-  const worksSlider = new Swiper('.works__slider', {
-    slidesPerGroup: 1,
-    slidesPerView: 1,
-    spaceBetween: 8,
-    loop: true,
-    grabCursor: true,
-    simulateTouch: true,
-    watchOverflow: true,
-    speed: 600,
-    mousewheel: {
-      forceToAxis: true,
-      sensitivity: 1,
-      releaseOnEdges: true,
-    },
-    touchEvents: {
-      prevent: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: false,
-    breakpoints: {
-      835: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        pagination: false,
-        navigation: {
-          prevEl: ".works-button-prev",
-          nextEl: ".works-button-next",
+    const personalSlider = new Swiper('.personal__slider', {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      grabCursor: true,
+      simulateTouch: true,
+      watchOverflow: true,
+      speed: 1000,
+      // effect: "fade",
+      // effect: "flip",
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          translate: ["-20%", 0, -1],
+        },
+        next: {
+          translate: ["20%", 0, 0],
         },
       },
+      mousewheel: {
+        forceToAxis: true,
+        sensitivity: 1,
+        releaseOnEdges: true
+      },
+      touchEvents: {
+        prevent: true
+      },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    });
+
+    const worksSlider = new Swiper('.works__slider', {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 8,
+      loop: true,
+      grabCursor: true,
+      simulateTouch: true,
+      watchOverflow: true,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+        sensitivity: 1,
+        releaseOnEdges: true,
+      },
+      touchEvents: {
+        prevent: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        prevEl: ".works-button-prev",
+        nextEl: ".works-button-next",
+      },
+      breakpoints: {
+        835: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+          pagination: false,
+        },
+      },
+    });
+  }
+
+  /**
+   * GSAP
+   */
+  const fadeUps = document.querySelectorAll('[data-transform="fadeUp"]');
+  if (fadeUps.length > 0) {
+    fadeUps.forEach(fadeUp => {
+      const tl = gsap.timeline({
+        paused: true
+      });
+      tl.from(fadeUp, {
+        opacity: 0,
+        y: "100",
+        duration: .5,
+        ease: "ease",
+        stagger: {
+          amount: .3
+        },
+      });
+      scrollTriggerPlayer(fadeUp, tl)
+    });
+  }
+
+  const titleFadeUps = document.querySelectorAll('[data-transform="titleFadeUp"]');
+  if (titleFadeUps.length > 0) {
+    titleFadeUps.forEach(titleFadeUp => {
+      const tl = gsap.timeline({
+        paused: true
+      });
+      const title = titleFadeUp.querySelector('[data-gradient="gradientMove"]');
+      tl.from(title, {
+        opacity: 0,
+        y: "100",
+        duration: .5,
+        ease: "ease",
+        stagger: {
+          amount: .3
+        },
+      });
+      scrollTriggerPlayer(title, tl)
+    });
+  }
+
+  const directionsItems = document.querySelector('.directions__items');
+  const directionsItem1 = document.querySelector('[data-transform="foreachFadeUp1"]');
+  const directionsItem2 = document.querySelector('[data-transform="foreachFadeUp2"]');
+  const directionsItem3 = document.querySelector('[data-transform="foreachFadeUp3"]');
+  const directionsItem4 = document.querySelector('[data-transform="foreachFadeUp4"]');
+
+  const tl = gsap.timeline({
+    paused: true
+  });
+
+  tl.to(directionsItem1, { duration: 0.3, y: 0, opacity: 1, stagger: { amount: .3 } })
+    .to(directionsItem2, { duration: 0.3, y: 0, opacity: 1, stagger: { amount: .3 } })
+    .to(directionsItem3, { duration: 0.3, y: 0, opacity: 1, stagger: { amount: .3 } })
+    .to(directionsItem4, { duration: 0.3, y: 0, opacity: 1, stagger: { amount: .3 } })
+  scrollTriggerPlayer(directionsItems, tl);
+
+  function scrollTriggerPlayer(triggerElement, timeline, onEnterStart = "top 95%") {
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top bottom",
+      onLeaveBack: () => {
+        timeline.progress(1);
+        timeline.pause()
+      }
+    });
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: onEnterStart,
+      scrub: true,
+      onEnter: () => timeline.play()
+    })
+  }
+
+  // $(window).on('resize load', function () {
+
+  //   /* Анимация mask */
+  //   $(".wrapper").mousemove(function (e) {
+  //     const masks = document.querySelectorAll('.mask');
+  //     masks.forEach(mask => {
+  //       parallaxIt(e, mask, -300);
+  //     });
+  //   });
+
+  //   function parallaxIt(e, target, movement) {
+  //     var $this = $(target);
+  //     var relX = e.pageX - $this.offset().left;
+  //     var relY = e.pageY - $this.offset().top;
+
+  //     TweenMax.to(target, 1, {
+  //       x: (relX - $this.width() / 2) / $this.width() * movement,
+  //       y: (relY - $this.height() / 2) / $this.height() * movement,
+  //       ease: 'none'
+  //     });
+  //   }
+
+  // });
+
+  const parallaxImgBoxes = document.querySelectorAll('[data-animation="parallax-box"]');
+  if (parallaxImgBoxes != 0) {
+    parallaxImgBoxes.forEach(parallaxImgBox => {
+
+      gsap.fromTo(parallaxImgBox,
+        { y: '20%' },
+        {
+          y: '-20%',
+          scrollTrigger: {
+            trigger: parallaxImgBox,
+            start: 'top 90%',
+            end: 'bottom top',
+            scrub: true,
+          },
+        }
+      );
+
+    });
+  }
+
+  /**
+   * Инициализация Fabcybox
+   */
+  Fancybox.bind('[data-fancybox]', {
+    Html: {
+      autoSize: false,
     },
+    on: {
+      'Carousel.ready': () => {
+        lenis.stop();
+      },
+      destroy: () => {
+        lenis.start();
+      }
+    }
   });
 
 });
